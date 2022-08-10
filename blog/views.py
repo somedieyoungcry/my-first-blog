3be23240e1ae9e164caf7706724b01acaec6ab77
somedieyoungcry <jfuentess004@alumno.uaemex.mx>
2022-08-10 00:52:34 -0500

    Changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
+#+from .forms import PostForm
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all().order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
